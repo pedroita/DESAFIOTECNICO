0de43e8147f7946455b7523c31fa1f2aa97791ba
Pedro Ítalo <44596929+pedroita@users.noreply.github.com>
2024-02-21 10:35:12 -0300

    Update step1.py

--- Desafio 2/step1.py
+++ Desafio 2/step1.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import tabula
-
+import zipfile
 
 def downloadPDF(url, filename):
     response = requests.get(url)
@@ -16,27 +16,22 @@ soup = BeautifulSoup(page.text, 'html.parser')
 
 pdf_links = soup.find_all('a', href=lambda href: href and href.endswith('.pdf'))
 
-
 count = 1
 for link in pdf_links:
-    if 'Anexo I' in link.text: 
+    if 'Anexo' in link.text: 
         pdf_url = link['href']
         filename = f'Anexo {count}.pdf'  
         downloadPDF(pdf_url, filename)
         count += 1  
     
-print ("Extração realizada com sucesso!")
+print("Extração realizada com sucesso!")
 
 def gettable(pdf_file):
     tables = tabula.read_pdf(pdf_file, pages='all', multiple_tables=True)
     
-    for i, table in enumerate(tables):
-        print(f'Tabela {i+1}:')
-        print(table.df)
-    
     df_list = []
     for table in tables:
-        df_list.append(table.df)
+        df_list.append(table)  
     df = pd.concat(df_list)
     
     return df
@@ -49,7 +44,9 @@ csv_filename = 'dados_anexo1.csv'
 anexo1_df.to_csv(csv_filename, index=False)
 print(f'Dados extraídos do Anexo I e salvos em {csv_filename}')
 
+zip_filename = 'Teste_Pedro_italo_Campos.zip'
 
+with zipfile.ZipFile(zip_filename, 'w') as zipf:
+    zipf.write(csv_filename)
 
-
-
+print(f'Arquivo CSV adicionado ao arquivo ZIP: {zip_filename}')
